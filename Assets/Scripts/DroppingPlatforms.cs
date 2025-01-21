ff7f29b60488c03b37e48ccfc9c1c380ff7b4e0c
Julikrie <julian.kriegel96@gmail.com>
2025-01-21 12:17:32 +0100

    Adjusted Dropping Platforms
    - Changed Particle System
    - Added Respawn

--- Assets/Scripts/DroppingPlatforms.cs
+++ Assets/Scripts/DroppingPlatforms.cs
@@ -8,14 +8,16 @@ public class DroppingPlatforms : MonoBehaviour
     public GameObject PlatformCrumble;
     public float ShakeForce = 1f;
     public float DropDelay;
-    public float DestroyTime;
+    public float RespawnTime;
 
     private Rigidbody2D _rb;
+    private Vector3 _originalPosition;
     private CinemachineImpulseSource _impulseSource;
 
     public void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _originalPosition = transform.position;
         _impulseSource = GetComponent<CinemachineImpulseSource>();
 
         PlatformCrumble.SetActive(false);
@@ -26,11 +28,11 @@ public class DroppingPlatforms : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             StartCoroutine(DroppingPlatform());
+            StartCoroutine(RespawnPlatform());
 
             _impulseSource.GenerateImpulseWithForce(ShakeForce);
-            PlatformCrumble.SetActive(true);
 
-            Destroy(gameObject, DestroyTime);
+            PlatformCrumble.SetActive(true);
         }
         else
         {
@@ -41,6 +43,19 @@ public class DroppingPlatforms : MonoBehaviour
     private IEnumerator DroppingPlatform()
     {
         yield return new WaitForSeconds(DropDelay);
+
         _rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    private IEnumerator RespawnPlatform()
+    {
+        yield return new WaitForSeconds(RespawnTime);
+
+        transform.position = _originalPosition;
+
+        _rb.bodyType = RigidbodyType2D.Kinematic;
+        _rb.velocity = Vector3.zero;
+
+        gameObject.SetActive(true);
     }
 }
