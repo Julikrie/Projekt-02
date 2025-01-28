f876ab83163c1f792c1ed7261c6edabe0b1b867c
Julikrie <julian.kriegel96@gmail.com>
2025-01-28 18:11:37 +0100

    Added Respawn Script for Objects

---
+++ Assets/Scripts/RespawnObjects.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnObjects : MonoBehaviour
+{
+    public float RespawnTime;
+    private Vector3 _originalPosition;
+
+    private void Start()
+    {
+        _originalPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        RespawnPlatform();
+    }
+
+    private IEnumerator RespawnPlatform()
+    {
+        yield return new WaitForSeconds(RespawnTime);
+
+        transform.position = _originalPosition;
+
+        gameObject.SetActive(true);
+    }
+}
