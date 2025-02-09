4f2297750395c999ea96161b3519d4848b448236
Julikrie <julian.kriegel96@gmail.com>
2025-02-09 13:40:10 +0100

    Added in game Menu logic

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,9 +10,63 @@ public class MenuManager : MonoBehaviour
     public GameObject ControlBackground;
     public GameObject ReturnButton;
 
+    [SerializeField]
+    private bool _isPaused = false;
+    [SerializeField]
+    private bool _inGame;
+
     private void Start()
     {
+        CheckForCurrentScene();
+
+        if (_inGame)
+        {
+            MenuBackground.SetActive(false);
+        }
+        else
+        {
+            MenuBackground.SetActive(true);
+        }
+
         ControlBackground.SetActive(false);
+    }
+
+    private void Update()
+    {
+        StopGame();
+    }
+
+    private void StopGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _isPaused = !_isPaused;
+
+            if (_isPaused)
+            {
+                Time.timeScale = 0;
+                MenuBackground.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                MenuBackground.SetActive(false);
+            }
+        }
+    }
+
+    private void CheckForCurrentScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentScene == 1 || currentScene == 2 || currentScene == 3)
+        {
+            _inGame = true;
+        }
+        else
+        {
+            _inGame = false;
+        }
     }
 
     public void StartGame()
