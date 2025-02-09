3ba699a91965224cf1e0e4115f107f5e02087455
Julikrie <julian.kriegel96@gmail.com>
2025-02-09 12:26:43 +0100

    Added Menu Scene

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,9 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject MenuBackground;
+    public GameObject ControlBackground;
+    public GameObject ReturnButton;
+
+    private void Start()
+    {
+        ControlBackground.SetActive(false);
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Forest 01");
+    }
+
+    public void ShowControls()
+    {
+        MenuBackground.SetActive(false);
+        ControlBackground.SetActive(true);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        MenuBackground.SetActive(true);
+        ControlBackground.SetActive(false);
     }
 
     public void QuitGame()
